65b2528e3d99554e48491ddacc795c6e4d99f5b4
JuliusNiedermeier <business.niedermeier@gmail.com>
2020-09-23 14:28:16 +0200

    Simplified notifications store module

--- store/notifications.js
+++ store/notifications.js
@@ -11,26 +11,18 @@ export const mutations = {
     if (!idExists) state.stack.push(payload)
   },
 
-  hide(state, id) {
-    const index = state.stack.findIndex(
-      (notification) => notification.id === id
-    )
-    state.stack[index].hide = true
-  },
-
   remove(state, id) {
     const index = state.stack.findIndex(
       (notification) => notification.id === id
     )
-    state.stack.splice(index, 1)
+    if (index >= 0) state.stack.splice(index, 1)
   },
 }
 
 export const actions = {
   show({ commit }, payload) {
     if (!payload.id) payload.id = Date.now()
-    commit('add', { ...payload, hide: false })
-    setTimeout(() => commit('hide', payload.id), 5000)
-    setTimeout(() => commit('remove', payload.id), 6000)
+    commit('add', payload)
+    setTimeout(() => commit('remove', payload.id), 5000)
   },
 }
