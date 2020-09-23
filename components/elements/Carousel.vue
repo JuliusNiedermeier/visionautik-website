99afa991c83c273df9cae1d3c4680bb046a59e65
JuliusNiedermeier <business.niedermeier@gmail.com>
2020-09-23 14:23:48 +0200

    Recreated Carousel

--- components/elements/Carousel.vue
+++ components/elements/Carousel.vue
@@ -1,316 +1,143 @@
 <template>
-  <div class="carousel-component" :class="{overflow: overflow}">
-    <div class="carousel-component__track-wrapper" :style="`width: ${slideCount * 100}%;`">
-      <ul
-        class="carousel-component__track-wrapper__track noselect"
-        :class="{gap: gap}"
-        ref="carouselTrack"
-        :style="`width: ${100 / slidesPerFrame}%; transform: translateX(-${100 / slideCount * activeSlideIndex}%) translateX(${manualDrag}px)`"
-      >
-        <slot />
-      </ul>
-    </div>
-    <div class="carousel-component__navigation-controls">
-      <va-slider-navigation-controls
-        :number="slideCount < slidesPerFrame ? 1 : slideCount - offset"
-        :activeIndex="activeSlideIndex"
-        @prev="handleNavClick('prev')"
-        @next="handleNavClick('next')"
-        @indicator-clicked="handleIndicatorClick"
-      />
+  <div class="carousel-component" :class="{gap: gap}" ref="component">
+    <div
+      class="carousel-component__track"
+      :class="{dragging: dragInitialClientX !== null}"
+      ref="track"
+      :style="`transform: translateX(${offset + drag}px)`"
+    >
+      <slot />
     </div>
   </div>
 </template>
 
 <script>
-import Icon from './Icon'
-import SliderNavigationControls from './SliderNavigationControls'
 export default {
-  name: 'va-carousel',
-
-  components: {
-    'va-icon': Icon,
-    'va-slider-navigation-controls': SliderNavigationControls,
-  },
-
   props: {
-    randomize: Boolean,
-    overflow: Boolean,
     gap: Boolean,
-    autoplayInterval: String,
-    loop: Boolean,
-    slidesPerFrame: {
-      type: String | Number,
-      required: false,
-      default: 1,
-    },
+    fullwidth: Boolean,
   },
 
   data() {
     return {
-      activeSlideIndex: null,
-      slideCount: 0,
-      initialXPosition: null,
-      manualDrag: 0,
-      autoplayIntervalId: null,
-      offset: null,
+      offset: 0,
+      drag: 0,
+      dragInitialClientX: null,
+      reachedStart: true,
+      reachedEnd: false,
     }
   },
 
-  watch: {
-    activeSlideIndex(index, previousIndex) {
-      this.offset = this.slidesPerFrame - 1
-      if (this.checkIfMobileModeIsActive()) this.offset = 0
-
-      if (
-        !(this.slideCount < this.slidesPerFrame) &&
-        index + this.offset > this.slideCount - 1
-      ) {
-        if (this.loop) this.activeSlideIndex = 0
-        else {
-          this.activeSlideIndex = previousIndex
-          this.stopAutoplay()
-        }
-      }
-
-      if (index < 0) {
-        if (this.loop) this.activeSlideIndex = this.slideCount - 1
-        else {
-          this.activeSlideIndex = previousIndex
-          this.stopAutoplay()
-        }
-      }
-
-      // this.activateSlides()
-    },
-  },
+  watch: {},
 
   methods: {
-    activateSlides() {
-      const slides = this.$refs.carouselTrack.children
-      const activeSlides = []
-      for (let i = 0; i < this.offset + 1; i++) {
-        activeSlides.push(this.activeSlideIndex + i)
-      }
-
-      for (let i = 0; i < this.slideCount; i++) {
-        if (activeSlides.includes(i)) {
-          slides[i].classList.add('active')
-        } else slides[i].classList.remove('active')
-      }
+    handleMouseDown(e) {
+      this.dragInitialClientX = e.clientX
+      this.$refs.track.addEventListener('mousemove', this.handleMouseMove)
+    },
+    handleTouchStart(e) {
+      // e.preventDefault() // Performance issues on android
+      this.dragInitialClientX = e.touches[0].clientX
+      this.$refs.track.addEventListener('touchmove', this.handleTouchMove)
     },
 
-    handleNavClick(direction) {
-      if (!/^prev$||^next$/.test(direction)) return
-      if (direction == 'prev') this.activeSlideIndex--
-      if (direction == 'next') this.activeSlideIndex++
-      this.stopAutoplay()
+    handleMouseMove(e) {
+      e.preventDefault()
+      this.drag = e.clientX - this.dragInitialClientX
+    },
+    handleTouchMove(e) {
+      // e.preventDefault() // Performance issues on android
+      this.drag = e.touches[0].clientX - this.dragInitialClientX
     },
 
-    handleIndicatorClick(index) {
-      this.activeSlideIndex = index
-      this.stopAutoplay()
-    },
-
-    handleMouseDown(mouseEvent) {
-      this.mouseDown = true
-      this.initialXPosition = mouseEvent.x
-      this.$refs.carouselTrack.classList.add('scrolling')
-
-      this.$refs.carouselTrack.addEventListener(
-        'mousemove',
-        this.handleMouseMove
-      )
-
-      this.$refs.carouselTrack.addEventListener(
-        'touchmove',
-        this.handleTouchMove
-      )
-    },
-
-    handleMouseUp(mouseEvent) {
-      this.mouseDown = false
-      this.initialXPosition = null
-      this.$refs.carouselTrack.classList.remove('scrolling')
-
-      mouseEvent.target
-        .querySelector('[grabbing="true"]')
-        .removeAttribute('grabbing')
-
-      this.$refs.carouselTrack.removeEventListener(
-        'mousemove',
-        this.handleMouseMove
-      )
-
-      this.$refs.carouselTrack.removeEventListener(
-        'touchmove',
-        this.handleTouchMove
-      )
-
+    handleMouseUp() {
+      this.dragInitialClientX = null
+      this.offset += this.drag
+      this.drag = 0
+      this.$refs.track.removeEventListener('mousemove', this.handleMouseMove)
       this.snapIntoNearestSlide()
-      this.manualDrag = 0
+    },
+    handleTouchEnd() {
+      this.$refs.track.removeEventListener('touchmove', this.handleTouchMove)
+      this.handleMouseUp()
     },
 
-    handleMouseMove(mouseEvent) {
-      const XPositionDifference = mouseEvent.x - this.initialXPosition
-      this.manualDrag = XPositionDifference
-
-      const grabbingSlide = mouseEvent.path.find(
-        (element) => element.tagName === 'LI'
-      )
-      if (grabbingSlide) grabbingSlide.setAttribute('grabbing', 'true')
+    handleMouseLeave() {
+      this.handleMouseUp()
     },
-
-    handleTouchStart(touchEvent) {
-      this.handleMouseDown({ x: touchEvent.touches[0].pageX })
-    },
-
-    handleTouchMove(touchEvent) {
-      this.handleMouseMove({
-        x: touchEvent.touches[0].pageX,
-        path: touchEvent.path,
-      })
-    },
-
-    handleTouchEnd(touchEvent) {
-      this.handleMouseUp({
-        x: this.manualDrag + this.initialXPosition,
-        target: touchEvent.path.find((element) =>
-          element.classList.contains('carousel-component__track-wrapper__track')
-        ),
-      })
+    handleTouchCancel() {
+      this.$refs.track.removeEventListener('touchmove', this.handleTouchMove)
+      this.handleMouseLeave()
     },
 
     snapIntoNearestSlide() {
-      const trackWidth = this.$refs.carouselTrack.offsetWidth
-      const slideWidth = trackWidth / this.slideCount
-      const trackOffset = slideWidth * this.activeSlideIndex
-      const draggedTrackOffset = trackOffset - this.manualDrag
-      const newIndexDec = (this.slideCount * draggedTrackOffset) / trackWidth
-      const deviation = newIndexDec - this.activeSlideIndex
+      let smallestOffsetLeft
+      for (const slide of this.$refs.track.children) {
+        const slideOffsetLeft =
+          slide.getBoundingClientRect().left -
+          this.$refs.component.getBoundingClientRect().left
 
-      if (deviation > 0.1) {
-        // console.log('Diviation is greater than 0.2')
-        this.activeSlideIndex = Math.ceil(newIndexDec)
-        this.stopAutoplay()
-        return
+        if (
+          typeof smallestOffsetLeft === 'undefined' ||
+          Math.abs(slideOffsetLeft) <= Math.abs(smallestOffsetLeft)
+        ) {
+          smallestOffsetLeft = slideOffsetLeft
+        }
       }
+      this.offset += -smallestOffsetLeft
 
-      if (deviation < -0.1) {
-        // console.log('Diviation is smaller than -0.2')
-        this.activeSlideIndex = Math.floor(newIndexDec)
-        this.stopAutoplay()
-        return
+      this.preventOverflow()
+    },
+
+    preventOverflow() {
+      const offsetRight =
+        this.$refs.track.getBoundingClientRect().right -
+        this.$refs.component.getBoundingClientRect().right
+
+      if (this.offset !== 0 && offsetRight < 0) {
+        this.offset = -(
+          this.$refs.track.offsetWidth - this.$refs.component.offsetWidth
+        )
       }
-    },
-
-    goToNextSlide() {
-      this.activeSlideIndex++
-    },
-
-    startAutoplay() {
-      this.autoplayIntervalId = setInterval(
-        this.goToNextSlide,
-        this.autoplayInterval
-      )
-    },
-
-    stopAutoplay() {
-      clearInterval(this.autoplayIntervalId)
-    },
-
-    checkIfMobileModeIsActive() {
-      const carouselTrack = this.$refs.carouselTrack
-      const carouselTrackWrapper = carouselTrack.closest(
-        '.carousel-component__track-wrapper'
-      )
-      const expectedTrackWidth = Math.round(
-        (carouselTrackWrapper.offsetWidth / 100) *
-          parseFloat(carouselTrack.style.width)
-      )
-      const actualTrackWidth = carouselTrack.offsetWidth
-      return expectedTrackWidth !== actualTrackWidth
-    },
-
-    handleTrackResize() {
-      console.log('Tracksize changed')
     },
   },
 
   mounted() {
-    const carouselTrack = this.$refs.carouselTrack
-    this.slideCount = carouselTrack.children.length
+    this.$refs.track.addEventListener('click', (e) => e.stopPropagation())
 
-    if (this.randomize) {
-      this.activeSlideIndex = Math.floor(Math.random() * this.slideCount)
-    } else {
-      this.activeSlideIndex = 0
-    }
+    this.$refs.track.addEventListener('mousedown', this.handleMouseDown)
+    this.$refs.track.addEventListener('touchstart', this.handleTouchStart)
 
-    if (this.autoplayInterval > 0) this.startAutoplay()
+    this.$refs.track.addEventListener('mouseup', this.handleMouseUp)
+    this.$refs.track.addEventListener('touchend', this.handleTouchEnd)
 
-    carouselTrack.addEventListener('mousedown', this.handleMouseDown)
-    carouselTrack.addEventListener('mouseup', this.handleMouseUp)
-    carouselTrack.addEventListener('touchstart', this.handleTouchStart)
-    carouselTrack.addEventListener('touchend', this.handleTouchEnd)
-
-    let carouselTrackObserver = new MutationObserver(() => {
-      this.slideCount = carouselTrack.children.length
-      this.activateSlides()
-    })
-    carouselTrackObserver.observe(carouselTrack, {
-      childList: true,
-    })
+    this.$refs.track.addEventListener('mouseleave', this.handleMouseLeave)
+    this.$refs.track.addEventListener('touchcancel', this.handleTouchCancel)
   },
 }
 </script>
 
 <style lang="scss" scoped>
 .carousel-component {
-  position: relative;
-  overflow-x: hidden;
-
-  &.overflow {
-    overflow-x: visible;
-  }
-
-  &__track-wrapper {
-    &__track {
-      margin: 0;
-      padding: 0;
-      list-style: none;
-      display: flex;
-      justify-content: space-between;
-      transition: transform $duration--fast ease;
-
-      cursor: grab;
-
-      &:active {
-        cursor: grabbing;
-      }
-
-      &.scrolling {
-        transition: none;
-      }
-
-      > li {
-        flex-basis: 100%;
-
-        &[grabbing='true'] {
-          pointer-events: none;
-        }
-      }
-
-      &.gap {
-        > li + li {
-          margin-left: 2rem;
-        }
-      }
-
-      @include mobile {
-        width: 100% !important;
-      }
+  &.gap {
+    * + * {
+      margin-left: 2rem;
     }
   }
+
+  &__track {
+    width: min-content;
+    display: flex;
+    justify-content: flex-start;
+    transition: transform $duration--fast ease;
+
+    &.dragging {
+      transition: none;
+    }
+  }
+}
+
+.item + .item {
+  margin-left: 2rem;
 }
 </style>
