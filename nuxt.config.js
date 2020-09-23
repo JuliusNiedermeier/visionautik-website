c0bdfda71a97d1445f97882d572dabc0baefcaf1
JuliusNiedermeier <business.niedermeier@gmail.com>
2020-09-23 14:20:59 +0200

    Updated Nuxt configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,12 +24,36 @@ export default {
         content: process.env.npm_package_description || '',
       },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'preconnect', href: 'https://app.snipcart.com' },
+      { rel: 'preconnect', href: 'https://cdn.snipcart.com' },
+      {
+        rel: 'stylesheet',
+        href: 'https://cdn.snipcart.com/themes/v3.0.16/default/snipcart.css',
+      },
+    ],
+    script: [
+      { src: 'https://cdn.snipcart.com/themes/v3.0.16/default/snipcart.js' },
+    ],
   },
   /*
    ** Customize the progress-bar color
    */
   loading: { color: 'blue' },
+
+  router: {
+    extendRoutes(routes, resolve) {
+      // Configure type param before offer single page
+      routes = routes.map((route) => {
+        if (route.name === 'offers-offer') {
+          route.name = 'offers-type-offer'
+          route.path = '/offers/:type/:offer'
+        }
+        return route
+      })
+    },
+  },
   /*
    ** Global CSS
    */
@@ -37,6 +61,7 @@ export default {
     '@/assets/scss/main.scss',
     '@/assets/scss/classes.scss',
     '@/assets/scss/fonts.scss',
+    '@/assets/scss/snipcart.scss',
   ],
   /*
    ** Plugins to load before mounting the App
@@ -86,15 +111,19 @@ export default {
         code: 'de',
         iso: 'de-DE',
         name: 'Deutsch',
+        file: 'de-DE.js',
       },
       {
         code: 'en',
         iso: 'en-GB',
         name: 'English',
+        file: 'en-GB.js',
       },
     ],
+    langDir: './lang/',
+    lazy: true,
     defaultLocale: 'de',
-    vueI18nLoader: true,
+    // vueI18nLoader: true,
     parsePages: false,
     seo: false,
     strategy: 'no_prefix',
@@ -113,4 +142,5 @@ export default {
   //   publicPath: '/assets/',
   //   extractCss: true,
   // },
+  globalName: 'nuxt',
 }
