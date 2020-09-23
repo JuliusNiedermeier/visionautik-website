53be744ed841f76e00e993bba35e9e49b9ad5af6
JuliusNiedermeier <business.niedermeier@gmail.com>
2020-09-23 14:36:37 +0200

    Updated notifications component

--- components/layout-parts/Notifications.vue
+++ components/layout-parts/Notifications.vue
@@ -4,11 +4,11 @@
       v-for="notification in notifications"
       :key="notification.id"
       class="notification-bar-component__notification"
-      :class="{hide: notification.hide, error: notification.error}"
+      :class="{error: notification.error}"
     >
       <div class="notification-bar-component__notification__body">
-        <p>{{notification.message}}</p>
-        <va-icon name="play" />
+        <small>{{notification.message}}</small>
+        <va-icon name="close" />
       </div>
     </div>
   </div>
@@ -29,48 +29,42 @@ export default {
 
 <style lang="scss" scoped>
 .notification-bar-component {
+  z-index: 1;
+  position: fixed;
+  display: flex;
+  justify-content: strech;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  pointer-events: none;
+
+  @include desktops {
+    bottom: 2rem;
+    justify-content: center;
+  }
+
   &__notification {
-    background-color: $color--blue--base;
+    padding: 2rem;
+    background-color: $color--lilac--base;
 
     &.error {
       background-color: $color--red--base;
     }
 
     &.hide {
-      animation-name: rollUp;
-      transform: scaleY(0);
-    }
-
-    animation-name: rollDown;
-    animation-duration: $duration--fast;
-    animation-timing-function: ease;
-
-    transform-origin: top;
-
-    @keyframes rollDown {
-      from {
-        transform: scaleY(0);
-      }
-      to {
-        transform: scaleY(1);
-      }
-    }
-
-    @keyframes rollUp {
-      from {
-        transform: scaleY(1);
-      }
-      to {
-        transform: scaleY(0);
-      }
+      transform: translateY(-100%);
+      opacity: 0;
     }
 
     &__body {
-      @include page-margin;
       display: flex;
       justify-content: space-between;
       align-items: center;
-      color: $color--grey--light;
+      color: $color--blue--base;
+
+      > * + * {
+        margin-left: 2rem;
+      }
     }
   }
 }
