35e3418c047774a97b79e0ce4e069b8804f6bcb7
JuliusNiedermeier <business.niedermeier@gmail.com>
2020-11-04 17:47:42 +0100

    Created component that displays all notifications

---
+++ components/layout-parts/header/Notifications/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="notification-bar-component invert-color">
+    <va-notification
+      v-for="notification in notifications"
+      :key="notification.id"
+      :notification="notification"
+    />
+  </div>
+</template>
+
+<script>
+import Notification from './Notification'
+export default {
+  name: 'va-notification-bar',
+  components: { 'va-notification': Notification },
+  computed: {
+    notifications() {
+      return this.$store.state.notifications.stack
+    },
+  },
+}
+</script>
