1e3ce6f9e6169f1ba95e599be304d2a19be03611
JuliusNiedermeier <business.niedermeier@gmail.com>
2020-11-04 21:14:11 +0100

    Reduced background opacity for image gallery fullscreen

--- components/slices/ImageGallery.vue
+++ components/slices/ImageGallery.vue
@@ -113,7 +113,7 @@ export default {
     left: 0;
     right: 0;
     bottom: 0;
-    background-color: transparentize($color--grey--dark, $amount: 0.03);
+    background-color: transparentize($color--grey--dark, $amount: 0.1);
     z-index: 1;
     opacity: 0;
     transition: opacity $duration--fast ease;
