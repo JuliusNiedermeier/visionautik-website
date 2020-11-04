45aa052af0083204f9723139c0fa823c262ec154
JuliusNiedermeier <business.niedermeier@gmail.com>
2020-11-04 17:41:49 +0100

    Adjusted types definition to changes in backend
    Course and Event type are now one unified type called Offer.
    Course and Events are distinguished by a category field as already done with the product type.

--- plugins/api/types.js
+++ plugins/api/types.js
@@ -1,33 +1,37 @@
 export const pages = {
-  index: 'index__page',
-  about: 'about__page',
-  history: 'history__page',
+  index: { typeName: 'index__page' },
+  about: { typeName: 'about__page' },
+  history: { typeName: 'history__page' },
 }
 export const collections = {
-  offers: 'offers__collection',
-  blog: 'blog__collection',
-  projects: 'projects__collection',
-  archive: 'archive__collection',
+  offers: { typeName: 'offers__collection' },
+  blog: { typeName: 'blog__collection' },
+  projects: { typeName: 'projects__collection' },
+  archive: { typeName: 'archive__collection' },
 }
 export const singles = {
-  coursePage: 'course__single',
-  eventPage: 'event__single',
-  productPage: 'product__single',
-  blogPostPage: 'blog_post__single',
-  projectPage: 'project__single',
-  teamMemberPage: 'team_member__single',
+  coursePage: { typeName: 'course__single' },
+  eventPage: { typeName: 'event__single' },
+  productPage: { typeName: 'product__single' },
+  blogPostPage: { typeName: 'blog_post__single' },
+  projectPage: { typeName: 'project__single' },
+  teamMemberPage: { typeName: 'team_member__single' },
 }
 export const repeatables = {
-  course: 'course__repeatable',
-  event: 'event__repeatable',
-  product: 'product__repeatable',
-  blogPost: 'blog_post__repeatable',
-  project: 'project__repeatable',
-  teamMember: 'team_member__repeatable',
-  organization: 'organization__repeatable',
-  pressArticle: 'press_article__repeatable',
-  pressQuote: 'press_quote__repeatable',
-  testimonial: 'testimonial__repeatable',
+  offer: { typeName: 'offer__repeatable', categories: ['course', 'event'] },
+  course: { typeName: 'course__repeatable' },
+  event: { typeName: 'event__repeatable' },
+  product: {
+    typeName: 'product__repeatable',
+    categories: ['book', 'merchandise', 'download'],
+  },
+  blogPost: { typeName: 'blog_post__repeatable' },
+  project: { typeName: 'project__repeatable' },
+  teamMember: { typeName: 'team_member__repeatable' },
+  organization: { typeName: 'organization__repeatable' },
+  pressArticle: { typeName: 'press_article__repeatable' },
+  pressQuote: { typeName: 'press_quote__repeatable' },
+  testimonial: { typeName: 'testimonial__repeatable' },
 }
 
 export default { pages, collections, singles, repeatables }
