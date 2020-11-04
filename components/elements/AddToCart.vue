579e7ed22d69610b036fbab01583874cbd1bf319
JuliusNiedermeier <business.niedermeier@gmail.com>
2020-11-04 16:51:26 +0100

    Extended AddToCartButton
    AddToCartButton.vue is now called AddToCart.vue.
    
    The component now also displays product options and the current price.

---
+++ components/elements/AddToCart.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="add-to-cart-component">
+    <div class="add-to-cart-component__custom-fields">
+      <select
+        v-for="customField in customFields"
+        :key="customField.key"
+        :data-custom-field-key="customField.key"
+        @change="handleCustomFieldSelectionChange"
+      >
+        <option disabled selected value hidden>Select an option</option>
+        <option
+          v-for="option in customField.options"
+          :key="option.name"
+          :selected="$route.query[customField.key] === option.name"
+        >
+          {{ option.name }}
+        </option>
+      </select>
+    </div>
+    <div class="add-to-cart-component__price" v-if="currentPrice">
+      {{ $intlFormatter.currency(currentPrice) }}
+    </div>
+    <button
+      class="add-to-cart-component__button snipcart-add-item"
+      :disabled="!currentPrice"
+      v-bind="{
+        ...$snipcart.bindProduct({
+          id,
+          price: basePrice,
+          storeUrl: url,
+          name,
+          description,
+        }),
+        ...$snipcart.customfields(customFieldsFormatted),
+      }"
+    >
+      Add to cart
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    disabled: Boolean,
+    id: String,
+    url: String,
+    basePrice: Number | String,
+    name: String,
+    description: String,
+    image: String,
+    customFields: Array,
+  },
+
+  computed: {
+    customFieldsFormatted() {
+      const customFields = []
+      for (const customField of this.customFields) {
+        if (!customField.options.length > 0) continue
+        const fieldLabel = customField.label
+        const fieldOptions = customField.options
+          .map(
+            (option) =>
+              `${option.name}[${option.priceDifference > 0 ? '+' : ''}${
+                option.priceDifference
+              }]`
+          )
+          .join('|')
+        const fieldDefaultOption = this.$route.query[customField.key]
+        customFields.push({
+          name: fieldLabel,
+          options: fieldOptions,
+          value: fieldDefaultOption
+            ? fieldDefaultOption
+            : this.customFields[0].options[0].name,
+        })
+      }
+      return customFields
+    },
+
+    currentPrice() {
+      let currentPrice = this.basePrice
+      for (const customField of this.customFields) {
+        let selectedOption = customField.options.find(
+          (option) => option.name === this.$route.query[customField.key]
+        )
+        if (!selectedOption) return null
+        currentPrice += selectedOption.priceDifference
+      }
+      return currentPrice
+    },
+  },
+
+  methods: {
+    handleCustomFieldSelectionChange(event) {
+      this.$router.replace({
+        query: { [event.target.dataset.customFieldKey]: event.target.value },
+      })
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.add-to-cart-component {
+  &__custom-fields {
+    select {
+      width: 100%;
+      margin-bottom: 2rem;
+    }
+  }
+
+  &__price {
+    padding: 1rem 1.5rem;
+    background-color: $color--blue--light;
+    color: white;
+    font-size: 1.6rem;
+    text-align: center;
+  }
+
+  &__button {
+    width: 100%;
+  }
+}
+</style>
