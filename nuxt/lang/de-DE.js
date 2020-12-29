fd49dafa57f6a9b400dc1637ac9d0d5060d6141d
JuliusNiedermeier <business.niedermeier@gmail.com>
2020-12-29 16:05:40 +0100

    Updated locale files

--- nuxt/lang/de-DE.js
+++ nuxt/lang/de-DE.js
@@ -1,4 +1,4 @@
-import types from '@/plugins/cms/types'
+import types from '@/assets/js/types.js'
 
 export default {
   notifications: {
@@ -6,6 +6,8 @@ export default {
     messages: {
       languageNotAvailable:
         'Es tut uns leid, diese Seite ist noch nicht vollständig übersetzt!',
+      missingContent:
+        'Einige Inhalte dieser Seite sind aktuell leider nicht verfügbar!',
     },
   },
 
@@ -17,6 +19,21 @@ export default {
         about: 'Über uns',
         projects: 'Projekte',
         blog: 'Blog',
+      },
+      offersMegaMenu: {
+        allOffers: {
+          heading: 'Alle Angebote',
+          info:
+            'Sieh dir unsere Angebote für Changemaker, Unternehmen und Prozessbegleiter an.',
+        },
+        nextEvents: {
+          heading: 'Nächste Events',
+          info: 'Check out our upcoming events.',
+        },
+        archive: {
+          heading: 'Archiv',
+          info: 'Rückblick auf abgeschlossene Kurse und Events',
+        },
       },
     },
 
@@ -33,11 +50,49 @@ export default {
         submitButtonLabel: 'Newsletter abonieren',
       },
     },
+
+    pageNotFound: {
+      heading: 'Seite nicht gefunden',
+      body: 'Bitte vergewissere dich, dass die von dir eingegebene URL korrekt ist.',
+      buttonLabel: 'Zurück',
+    },
   },
 
   global: {
+    placeholders: {
+      courses: {
+        heading: 'Kein passender Kurs dabei?',
+        body:
+          'Wir benachrichtigen dich gerne sobald neue Kurse verfügbar sind!',
+        buttonLabel: 'Benachrichtigen',
+      },
+      events: {
+        heading: 'Kein passendes Event dabei?',
+        body: 'Wir benachrichtigen dich gerne sobald neue Events geplant sind!',
+        buttonLabel: 'Benachrichtigen',
+      },
+      products: {
+        heading: 'Kein passendes Produkt dabei?',
+        body:
+          'Wir benachrichtigen dich gerne sobald neue Produkte verfügbar sind!',
+        buttonLabel: 'Benachrichtigen',
+      },
+      searchResults: {
+        heading: 'Deine Suche ergab keine Ergebnisse...',
+        body:
+          'Wir benachrichtigen dich aber gerne sobald neue Angebote verfügbar sind!',
+        buttonLabel: 'Benachrichtigen',
+      },
+    },
+
     auxiliaries: {
       // for: 'for',
+    },
+    ecommerce: {
+      free: 'Kostenlos',
+      notAvailable: 'Nicht verfügbar',
+      addToCart: 'In den Warenkorb',
+      toEventbrite: 'Weiter zu Eventbrite',
     },
     targetGroups: {
       changemakers: {
@@ -58,18 +113,40 @@ export default {
     },
   },
 
+  slices: {
+    pricingPlanList: {
+      nextEffectiveDateLabel: 'ab dem',
+      notAvailableYet: 'Noch nicht verfügbar',
+    },
+  },
+
   types: {
     // PAGES
     [types.pages.index.typeName]: {
       coursesSection: {
         allCourses: 'Alle Kurse',
-        placeholderHeading: 'Kein passender Kurs dabei?',
-        placeholderBody:
-          'Wir benachrichtigen dich gerne sobald neue Kurse verfügbar sind"!',
-        notify: 'Benachrichtigen',
+      },
+      eventsSection: {
+        allEvents: 'Alle Events',
+      },
+      teamSection: {
+        allTeamMembers: 'Alle Mitglieder und Unterstützer',
       },
       ourVisionSection: {
         readMore: 'Mehr erfahren',
+      },
+    },
+
+    [types.pages.about.typeName]: {
+      contactForm: {
+        name: 'Name',
+        email: 'Deine Email-Addresse',
+        subject: 'Betreff',
+        message: 'Deine Nachricht an uns...',
+        submit: 'Email senden',
+        successMessage: 'Danke! Wir haben deine Nachricht erhalten.',
+        errorMessage:
+          'Beim senden deiner Nachricht ist etwas schiefgelaufen! Versuche es bitte später noch einmal.',
       },
     },
 
@@ -77,29 +154,6 @@ export default {
     [types.collections.offers.typeName]: {
       searchBarPlaceholder: 'Suchen...',
       allTargetGroupsLabel: 'Alle',
-      courseResultsPlaceholder: {
-        heading: 'Kein passender Kurs dabei?',
-        body:
-          'Wir benachrichtigen dich gerne sobald neue Kurse verfügbar sind!',
-        buttonLabel: 'Benachrichtigen',
-      },
-      eventResultsPlaceholder: {
-        heading: 'Kein passendes Event dabei?',
-        body: 'Wir benachrichtigen dich gerne sobald neue Events geplant sind!',
-        buttonLabel: 'Benachrichtigen',
-      },
-      productResultsPlaceholder: {
-        heading: 'Kein passendes Produkt dabei?',
-        body:
-          'Wir benachrichtigen dich gerne sobald neue Produkte verfügbar sind!',
-        buttonLabel: 'Benachrichtigen',
-      },
-      resultsPlaceholder: {
-        heading: 'Deine Suche ergab keine Ergebnisse...',
-        body:
-          'Wir benachrichtigen dich aber gerne sobald neue Angebote verfügbar sind!',
-        buttonLabel: 'Benachrichtigen',
-      },
     },
     [types.collections.blog.typeName]: {},
     [types.collections.projects.typeName]: {},
@@ -110,45 +164,32 @@ export default {
     [types.singles.productPage.typeName]: {},
     [types.singles.blogPostPage.typeName]: {},
     [types.singles.projectPage.typeName]: {},
-    [types.singles.teamMemberPage.typeName]: {},
+    [types.singles.teamMemberPage.typeName]: {
+      references: {
+        website: 'Webseite',
+        email: 'Email',
+      },
+    },
 
     // REPEATABLES
-    [types.repeatables.offer.typeName]: {
-      singular: 'Angebot',
-      plural: 'Angebote',
+    [types.repeatables.activity.typeName]: {
+      label: 'Angebot | Angebote | {count} Angebote',
       categories: {
-        course: {
-          singular: 'Kurs',
-          plural: 'Kurse',
-        },
-        event: {
-          singular: 'Event',
-          plural: 'Events',
-        },
+        course: 'Kurs | Kurse | {count} Kurse',
+        event: 'Event | Events | {count} Events',
       },
+      choosePricingPlan: 'Angebot wählen',
     },
     [types.repeatables.product.typeName]: {
-      singular: 'Produkt',
-      plural: 'Produkte',
+      label: 'Produkt | Produkte | {count} Produkte',
       categories: {
-        book: {
-          singular: 'Buch',
-          plural: 'Bücher',
-        },
-        merchandise: {
-          singular: 'Merchandise',
-          plural: 'Merchandise',
-        },
-        download: {
-          singular: 'Download',
-          plural: 'Downloads',
-        },
+        book: 'Buch | Bücher | {count} Bücher',
+        merchandise: 'Merchandise | Merchandise | {count} Merchandise',
+        download: 'Download | Downloads | {count} Downloads',
       },
     },
-    [types.repeatables.blogPost.typeName]: {
-      singular: 'Blogbeitrag',
-      plural: 'Blogbeiträge',
-    },
+    [types.repeatables.blogPost.typeName]:
+      'Blogbeitrag | Blogbeiträge | {count} Blogbeiträge',
     [types.repeatables.teamMember.typeName]: {
       readMore: 'Profil ansehen',
     },
--- nuxt/lang/en-GB.js
+++ nuxt/lang/en-GB.js
@@ -1,10 +1,11 @@
-import types from '@/plugins/cms/types'
+import types from '@/assets/js/types.js'
 
 export default {
   notifications: {
     close: 'close',
     messages: {
       languageNotAvailable: 'Sorry, this page is not fully translated yet!',
+      missingContent: 'Some contents of this page are currently not available!',
     },
   },
 
@@ -16,6 +17,21 @@ export default {
         about: 'About',
         projects: 'Projects',
         blog: 'Blog',
+      },
+      offersMegaMenu: {
+        allOffers: {
+          heading: 'All offers',
+          info:
+            'See all our offers for changemakers, businesses and facilitators.',
+        },
+        nextEvents: {
+          heading: 'Next events',
+          info: 'Check out our upcoming events.',
+        },
+        archive: {
+          heading: 'Archive',
+          info: "Look at what's been happening lately at Visionautics Academy.",
+        },
       },
     },
 
@@ -32,11 +48,47 @@ export default {
         submitButtonLabel: 'Subscribe to our newsletter',
       },
     },
+
+    pageNotFound: {
+      heading: 'Page not found',
+      body: 'Please make sure that the URL you have entered is correct.',
+      buttonLabel: 'back',
+    },
   },
 
   global: {
+    placeholders: {
+      courses: {
+        heading: 'No suitable course available?',
+        body: 'Would you like to be notified when new courses are available?',
+        buttonLabel: 'Notify',
+      },
+      events: {
+        heading: 'No suitable event available?',
+        body:
+          'Would you like to be notified as soon as new events are planned?',
+        buttonLabel: 'Notify',
+      },
+      products: {
+        heading: 'No suitable product available?',
+        body: 'Would you like to be notified when new products are available?',
+        buttonLabel: 'Notify',
+      },
+      searchResults: {
+        heading: 'No search results...',
+        body: 'Would you like to be notified when new offers are available?',
+        buttonLabel: 'Notify',
+      },
+    },
+
     auxiliaries: {
       // for: 'for',
+    },
+    ecommerce: {
+      free: 'Free',
+      notAvailable: 'Not available',
+      addToCart: 'Add to cart',
+      toEventbrite: 'Continue with Eventbrite',
     },
     targetGroups: {
       changemakers: {
@@ -57,18 +109,40 @@ export default {
     },
   },
 
+  slices: {
+    pricingPlanList: {
+      nextEffectiveDateLabel: 'after',
+      notAvailableYet: 'Not available yet',
+    },
+  },
+
   types: {
     // PAGES
     [types.pages.index.typeName]: {
       coursesSection: {
         allCourses: 'All courses',
-        placeholderHeading: 'No suitable course available?',
-        placeholderBody:
-          'Would you like to be notified when new courses are available?',
-        notify: 'Notify',
+      },
+      eventsSection: {
+        allEvents: 'All events',
+      },
+      teamSection: {
+        allTeamMembers: 'All members und supporters',
       },
       ourVisionSection: {
         readMore: 'Read more',
+      },
+    },
+
+    [types.pages.about.typeName]: {
+      contactForm: {
+        name: 'Name',
+        email: 'Your email address',
+        subject: 'Subject',
+        message: 'Your message...',
+        submit: 'send email',
+        successMessage: 'Thanks! We got your message.',
+        errorMessage:
+          'Something went wrong while sending your message! Please try again later.',
       },
     },
 
@@ -76,27 +150,6 @@ export default {
     [types.collections.offers.typeName]: {
       searchBarPlaceholder: 'Search...',
       allTargetGroupsLabel: 'All',
-      courseResultsPlaceholder: {
-        heading: 'No suitable course available?',
-        body: 'Would you like to be notified when new courses are available?',
-        buttonLabel: 'Notify',
-      },
-      eventResultsPlaceholder: {
-        heading: 'No suitable event available?',
-        body:
-          'Would you like to be notified as soon as new events are planned?',
-        buttonLabel: 'Notify',
-      },
-      productResultsPlaceholder: {
-        heading: 'No suitable product available?',
-        body: 'Would you like to be notified when new products are available?',
-        buttonLabel: 'Notify',
-      },
-      resultsPlaceholder: {
-        heading: 'No search results...',
-        body: 'Would you like to be notified when new offers are available?',
-        buttonLabel: 'Notify',
-      },
     },
     [types.collections.blog.typeName]: {},
     [types.collections.projects.typeName]: {},
@@ -107,45 +160,32 @@ export default {
     [types.singles.productPage.typeName]: {},
     [types.singles.blogPostPage.typeName]: {},
     [types.singles.projectPage.typeName]: {},
-    [types.singles.teamMemberPage.typeName]: {},
+    [types.singles.teamMemberPage.typeName]: {
+      references: {
+        website: 'website',
+        email: 'email',
+      },
+    },
 
     // REPEATABLES
     [types.repeatables.offer.typeName]: {
-      singular: 'offer',
-      plural: 'offers',
+      label: 'offer | offers | {count} offers',
       categories: {
-        course: {
-          singular: 'course',
-          plural: 'courses',
-        },
-        event: {
-          singular: 'event',
-          plural: 'events',
-        },
+        course: 'course | courses | {count} courses',
+        event: 'event | events | {count} events',
       },
+      choosePricingPlan: 'Choose a pricing plan',
     },
     [types.repeatables.product.typeName]: {
-      singular: 'product',
-      plural: 'products',
+      label: 'product | products | {count} products',
       categories: {
-        book: {
-          singular: 'book',
-          plural: 'books',
-        },
-        merchandise: {
-          singular: 'merchandise',
-          plural: 'merchandise',
-        },
-        download: {
-          singular: 'download',
-          plural: 'downloads',
-        },
+        book: 'book | books | {count} books',
+        merchandise: 'merch | merch | {count} merch',
+        download: 'download | downloads | {count} downloads',
       },
     },
-    [types.repeatables.blogPost.typeName]: {
-      singular: 'blog post',
-      plural: 'blog posts',
-    },
+    [types.repeatables.blogPost.typeName]:
+      'blog post | blog posts | {count} blog posts',
     [types.repeatables.teamMember.typeName]: {
       readMore: 'Visit profile',
     },
