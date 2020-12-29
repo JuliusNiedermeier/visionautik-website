d69eff72e7303a0d56f3d0b8800ed07d294063e5
JuliusNiedermeier <business.niedermeier@gmail.com>
2020-12-29 16:29:39 +0100

    added usecase

--- nuxt/mixins/usecases/fetchOne.js
+++ nuxt/mixins/usecases/fetchOne.js
@@ -3,10 +3,10 @@ import queryCms from '@/mixins/util/queryCms.js'
 export default {
   mixins: [queryCms],
   methods: {
-    fetchOne(type) {
+    fetchOne(type, fields = []) {
       return this.queryCms(
         [this.$prismic.predicates.at('document.type', type)],
-        { pageSize: 1 },
+        { pageSize: 1, fetch: fields },
         true
       )
     },
