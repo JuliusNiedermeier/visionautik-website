7d47e353ee16e617b58e0d022af3a5528423492a
JuliusNiedermeier <business.niedermeier@gmail.com>
2020-12-29 16:30:57 +0100

    updated link resolver

--- nuxt/plugins/link-resolver.js
+++ nuxt/plugins/link-resolver.js
@@ -1,19 +1,21 @@
+import { repeatables, pages } from '@/assets/js/types.js'
+
 /**
  * To learn more about Link Resolving check out the Prismic documentation
  */
 
 export default function (doc) {
-    if (doc.isBroken) {
-      return '/not-found'
-    }
-  
-    if (doc.type === 'blog_home') {
-      return '/'
-    }
-  
-    if (doc.type === 'post') {
-      return '/blog/' + doc.uid
-    }
-  
-    return '/not-found'
+  if (doc.isBroken) {
+    return '/404'
   }
+
+  if (doc.type === pages.index.typeName) {
+    return '/'
+  }
+
+  if (doc.type === repeatables.activity.typeName) {
+    return '/offers/activity/' + doc.uid
+  }
+
+  return '/404'
+}
