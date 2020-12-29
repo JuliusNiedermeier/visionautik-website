f2b8ee7f2d4a2f80b87386e9263cc2019c2dcdce
JuliusNiedermeier <business.niedermeier@gmail.com>
2020-12-29 16:25:23 +0100

    Added routes

--- nuxt/router.js
+++ nuxt/router.js
@@ -6,6 +6,9 @@ import Offers from '@/components/pages/Offers/Offers.vue'
 import Activity from '@/components/pages/Activity/Activity.vue'
 import Product from '@/components/pages/Product/Product.vue'
 import Cart from '@/components/pages/Cart/Cart.vue'
+import About from '@/components/pages/About/About.vue'
+import Playground from '@/components/pages/Playground/Playground.vue'
+import TeamMember from '@/components/pages/TeamMember/TeamMember.vue'
 
 Vue.use(Router)
 
@@ -14,7 +17,21 @@ export function createRouter() {
     mode: 'history',
     linkActiveClass: 'nuxt-link-active',
     linkExactActiveClass: 'nuxt-link-exact-active',
+    scrollBehavior(to, from, savedPosition) {
+      if (to.hash) {
+        return { selector: to.hash, behavior: 'smooth' }
+      } else if (savedPosition) {
+        return savedPosition
+      } else {
+        return { x: 0, y: 0 }
+      }
+    },
     routes: [
+      {
+        name: 'playground',
+        path: '/playground',
+        component: Playground,
+      },
       {
         name: 'home',
         path: '/',
@@ -32,12 +49,23 @@ export function createRouter() {
       },
       {
         name: 'product',
-        path: '/offers/product/:offer',
+        path: '/offers/product/:uid',
         component: Product,
       },
       {
+        name: 'cart',
         path: '/cart',
         component: Cart,
+      },
+      {
+        name: 'about',
+        path: '/about',
+        component: About,
+      },
+      {
+        name: 'team-member',
+        path: '/about/:uid',
+        component: TeamMember,
       },
     ],
   })
