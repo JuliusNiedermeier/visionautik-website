8c24a96242bd27cf55f4a0b41297bc4e2e72f219
JuliusNiedermeier <business.niedermeier@gmail.com>
2020-12-29 16:23:47 +0100

    Removed CMS Plugin

--- nuxt/plugins/cms/index.js
+++
@@ -1,9 +0,0 @@
-import query from './queryFactory'
-import types from '@/assets/js/types.js'
-import payment from './payment'
-
-export default (context, inject) => {
-  const cms = { Query: query(context), types, payment }
-  inject('cms', cms)
-  context.$cms = cms
-}
